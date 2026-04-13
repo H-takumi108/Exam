2170fc3e12e7cc7378d48590a6c23a79184d77c8
admin <admin@LAPTOP-13F62IK0>
2026-04-13 14:00:39 +0900

    Dao.java

---
+++ src/main/java/dao/Dao.java
@@ -0,0 +1,11 @@
+package dao;
+
+import java.sql.Connection;
+
+public class Dao {
+	static DateSourse ds;
+	
+	public Connection getConnection() throws Exception {
+		return ds.getConnection();
+	}
+}
