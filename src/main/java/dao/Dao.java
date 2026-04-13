707237e68d0d01d32b994a61432882c3bd0662cf
admin <admin@LAPTOP-13F62IK0>
2026-04-13 14:11:11 +0900

    Action.java

--- src/main/java/dao/Dao.java
+++ src/main/java/dao/Dao.java
@@ -4,8 +4,8 @@ import java.sql.Connection;
 
 public class Dao {
 	static DateSourse ds;
-	
+//データベース未接続なのでエラー	
 	public Connection getConnection() throws Exception {
 		return ds.getConnection();
 	}
-} 
+}
