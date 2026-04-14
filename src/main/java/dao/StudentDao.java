9622301f9aadd5bc66fc7f4c752a0d1fb23de267
admin <admin@LAPTOP-1QR3OJGT>
2026-04-14 10:14:45 +0900

    StudentDao.java

---
+++ src/main/java/dao/StudentDao.java
@@ -0,0 +1,33 @@
+package dao;
+
+import java.sql.ResultSet;
+
+public class StudentDao extends Dao {
+	
+	private Strings baseSql;
+	
+	public Student get(String no) throws Exception {
+		
+	}
+	
+	private List<Student> postFilter(ResultSet rSet. School school) throws Exception {
+		
+	}
+	
+	public List<Student> filter(School school. int entYear. String classNum. booleam isAttend) throws Exception {
+		
+	}
+	
+	public LIst<Student> filter(School school. int entYear. boolean isAttend) throws Exception {
+		
+	}
+	
+	public List<Student> filter(School school. boolean isAttend) throws Exception {
+		
+	}
+	
+	public boolean save(Student student) throws Exception {
+		
+	}
+	
+}
