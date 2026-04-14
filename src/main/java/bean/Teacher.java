4aafc2d09a914ab963bda1312603695be8eaf6f5
admin <admin@LAPTOP-13F62IK0>
2026-04-14 15:11:35 +0900

    studentlistaction途中

--- src/main/java/bean/Teacher.java
+++ src/main/java/bean/Teacher.java
@@ -10,7 +10,7 @@ public class Teacher implements java.io.Serializable {
 	public String getId() {
 		return id;
 	}
-
+ 
 	public String getPassword() {
 		return password;
 	}
@@ -38,4 +38,4 @@ public class Teacher implements java.io.Serializable {
 	public void setSchool(String school) {
 		this.school = school;
 	}
-}  
+}   
