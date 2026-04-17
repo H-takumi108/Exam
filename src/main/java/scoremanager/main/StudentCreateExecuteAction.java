11e9fb65646f73d5fc1f6876bece1663465e9766
admin <admin@LAPTOP-13F62IK0>
2026-04-17 10:42:13 +0900

    studentcreateaction.java

--- src/main/java/scoremanager/main/StudentCreateExecuteAction.java
+++ src/main/java/scoremanager/main/StudentCreateExecuteAction.java
@@ -1,6 +1,8 @@
 package scoremanager.main;
  
+import bean.Student;
 import bean.Teacher;
+import dao.StudentDao;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
@@ -13,27 +15,87 @@ public class StudentCreateExecuteAction extends Action {
     	HttpSession session = req.getSession();
     	Teacher teacher = (Teacher)session.getAttribute("user");
     	
+    	//変数の定義
     	String entYearStr = "";
     	String noStr = "";
     	String nameStr ="";
     	String classNumStr ="";
+    	int entYear = 0;
     	
+    	//jspからの入力
     	entYearStr = req.getParameter("entYearStr");
     	noStr = req.getParameter("noStr");
     	nameStr = req.getParameter("nameStr");
     	classNumStr = req.getParameter("classNumStr");
-    
+    	
+    	//エラーを未発生に設定
     	boolean error = false;
+    	// 学生番号が未入力なら
     	if (noStr == null) {
+    		//jspに表示
     	   session.setAttribute("error1", "このフィードを入力して下さい");
+    	   //エラーを発生に変更
+    	   error = true;
+    	} else {
+    		//入力されていたら重複していないかを確認
+    		StudentDao dao = new StudentDao();
+    	    Student student = dao.get(noStr);
+
+    	    //重複していたら
+    	    if (student != null) {
+    	    	//jspに表示
+    	        session.setAttribute("error1", "この学生番号が重複しています");
+    	      //エラーを発生に変更
+    	        error = true;
+    	    }
+    	}
+    	//学生氏名が未入力なら
+    	if (nameStr == null) {
+    		//jspに表示
+    		session.setAttribute("error2", "このフィードを入力して下さい");
+    		//エラーを発生に変更
     	   error = true;
     	}
-    	if (nameStr == null) {
-    		session.setAttribute("error2", "このフィードを入力して下さい");
-     	   error = true;
+    	//入学年度が未入力なら
+    	if (entYearStr == null) {
+    		//jspに表示
+    	    session.setAttribute("error3","入学年度を入力してください");
+    	  //エラーを発生に変更
+    	    error = true;
+    	} else {
+    		//入学年度をint型に変換
+    	    try {
+    	    	entYear = Integer.parseInt(entYearStr);
+    	    } catch (NumberFormatException e) {
+    	    	//jspに表示
+    	        session.setAttribute("error3", "入学年度は数字で入力してください");
+    	      //エラーを発生に変更
+    	        error = true;
+    	    }
     	}
-    	if (error = true) {
+    	//エラーが発生になっていたら
+    	if (error == true) {
+    		//入力用のjspに戻る
     		req.getRequestDispatcher("studentcreate.jsp").forward(req, res);
     	}
-        }
+    	//エラーが発生していなかったら
+    	if (error == false) {
+    		// 登録を行う
+    	    Student student = new Student();
+
+    	    student.setNo(noStr);
+    	    student.setName(nameStr);
+    	    student.setEntYear(entYear);
+    	    student.setClassNum(classNumStr);
+    	    student.setAttend(true);
+
+    	    student.setSchool(teacher.getSchool());
+
+    	    StudentDao dao = new StudentDao();
+    	    dao.save(student);
+    	    
+    	    //登録完了ページに移動
+    	    req.getRequestDispatcher("xxxxxx.jsp").forward(req, res);
+    	}
+    }
 }  
--- src/main/webapp/scoremanager/studentcreate.jsp
+++ src/main/webapp/scoremanager/studentcreate.jsp
@@ -3,10 +3,10 @@
 
 <h1>学生登録画面</h1>
 <form action="StudentCreateExecute.action" method="post">
-<p>入学年度<input type="text" name="entYearStr"></p>
-<p>学生番号<input type="text" name="noStr">${error1}</p>
-<p>学生氏名<input type="text" name="nameStr"></p>
-<p>クラス番号<input type="text" name="classNumStr"></p>
+<p>入学年度<input type="text" name="entYearStr">${error3}</p>
+<p>学生番号<input type="text" name="noStr" value="${noStr}">${error1}</p>
+<p>学生氏名<input type="text" name="nameStr" value="${nameStr}">${error2}</p>
+<p>クラス番号<input type="text" name="classNumStr" value="${classNumStr}"></p>
 <p>パスワード<input type="password" name="password"></p>
 <p><input type="submit" value="登録"></p>
 </form>
