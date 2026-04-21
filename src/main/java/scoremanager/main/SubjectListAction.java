d3a5480f0439619ccccec16cae3d956413fc72d9
admin <admin@LAPTOP-13F62IK0>
2026-04-21 11:15:52 +0900

    subjectlistaction

---
+++ src/main/java/scoremanager/main/SubjectListAction.java
@@ -0,0 +1,32 @@
+package scoremanager.main;
+ 
+import java.util.List;
+
+import bean.Subject;
+import bean.Teacher;
+import dao.SubjectDao;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import tool.Action;
+ 
+public class SubjectListAction extends Action {
+    public void execute(
+        HttpServletRequest request, HttpServletResponse response
+    ) throws Exception {
+    	HttpSession session = request.getSession();
+    	Teacher teacher = (Teacher)session.getAttribute("user");
+    	
+    	SubjectDao subjectdao = new SubjectDao();
+    	List<Subject> list = subjectdao.filter(teacher.getSchool());
+    	
+    	request.setAttribute("subjectlist",list );
+    	
+    	request.getRequestDispatcher("subject_list.jsp").forward(request, response);
+    	///jsp側では
+    	//<c:forEach var="subject" items="${subjectList}">
+        //${subject.name}
+        //</c:forEach>
+    	//のように記述
+    }
+}  
